5b9e175b6e3ca1b6697885a19bdfba1ba6bb9edb
Stelios G. Sfakianakis <sgsfak@gmail.com>
2026-03-15 12:38:42 +0200

    Revert "feat: change IntegerTable to have an in-memory implementation using ConcurrentHashMap"
    This reverts commit 49230c25277919c670ee208415daa681180a5e67.

--- source/java/org/rsna/ctp/stdstages/anonymizer/IntegerTable.java
+++ source/java/org/rsna/ctp/stdstages/anonymizer/IntegerTable.java
@@ -9,10 +9,6 @@ package org.rsna.ctp.stdstages.anonymizer;
 
 import java.io.File;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-
 import jdbm.RecordManager;
 import jdbm.htree.HTree;
 import org.apache.log4j.Logger;
@@ -26,9 +22,8 @@ public class IntegerTable {
 	static final Logger logger = Logger.getLogger(IntegerTable.class);
 
 	File dir;
-	final static ConcurrentHashMap<String,Integer> map = new ConcurrentHashMap<>();
-	final static ConcurrentHashMap<String,AtomicInteger> counterPerType = new ConcurrentHashMap<>();
-	// final static AtomicInteger globalCounter = new AtomicInteger(0);
+    public RecordManager recman = null;
+    public HTree index = null;
 
 	/**
 	 * Constructor; create an IntegerTable from a database file.
@@ -37,13 +32,26 @@ public class IntegerTable {
 	 */
 	public IntegerTable(File dir) throws Exception {
 		this.dir = dir;
+		File indexFile = new File(dir, "integers");
+		recman = JdbmUtil.getRecordManager( indexFile.getAbsolutePath() );
+		index = JdbmUtil.getHTree( recman, "index" );
+		if (index == null) {
+			logger.warn("Unable to load the integer database.");
+			throw new Exception("Unable to load the integer database.");
+		}
 	}
 
 	/**
 	 * Commit and close the IntegerTable.
 	 */
 	public void close() {
-		System.out.println("IntegerTable: close() called");
+		if (recman != null) {
+			try {
+				recman.commit();
+				recman.close();
+			}
+			catch (Exception ignore) { }
+		}
 	}
 
 	/**
@@ -55,27 +63,33 @@ public class IntegerTable {
 	 * @return the replacement string, with leading zeroes if necessary to pad the
 	 * replacement string to the required width.
 	 */
-	public String getInteger(String type, String text, int width) {
+	public synchronized String getInteger(String type, String text, int width) {
 		try {
-			final String trimmedText = text.trim();
-			final String trimmedType = type.trim();
-			logger.debug("getInteger: \""+trimmedType+"\", \""+trimmedText+"\", "+width);
-			String key = trimmedType + "/" + trimmedText;
+			text = text.trim();
+			type = type.trim();
+			logger.debug("getInteger: \""+type+"\", \""+text+"\", "+width);
+			String key = type + "/" + text;
 			logger.debug("...searching for "+key);
-			
-
-			Integer value = map.computeIfAbsent(key, k -> {
-				// int newValue = typeCounter.incrementAndGet();
-				final AtomicInteger typeCounter = counterPerType.computeIfAbsent(trimmedType, _k -> { return new AtomicInteger(0); });
-				int newValue = typeCounter.incrementAndGet();
-				logger.debug("IntegerTable: creating new value for "+k+": "+newValue);
-				return Integer.valueOf(newValue);
-			});
-			
-			
-
-			logger.debug("IntegerTable: "+ key + " => " + value);
-			logger.debug("...success");
+			Integer value = (Integer)index.get(key);
+			if (value == null) {
+				logger.debug("...got null");
+				String lastIntKey = "__" + type + "__";
+				logger.debug("...searching for "+lastIntKey);
+				Integer lastInt = (Integer)index.get(lastIntKey);
+				logger.debug("...got "+lastInt);
+				if (lastInt == null) lastInt = Integer.valueOf(0);
+				value = Integer.valueOf( lastInt.intValue() + 1 );
+				logger.debug("...storing "+value+" for "+lastIntKey);
+				index.put(lastIntKey, value);
+				logger.debug("...storing "+value+" for "+key);
+				index.put(key, value);
+				logger.debug("...success");
+				recman.commit();
+			}
+			else {
+				logger.debug("...got "+value);
+				logger.debug("...success");
+			}
 			int intValue = value.intValue();
 			String format = (width > 0) ? ("%0"+width+"d") : ("%d");
 			return String.format(format, intValue);
